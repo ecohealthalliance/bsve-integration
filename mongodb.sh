45216747fa8beaa8ac66dc1ddd94ef01b902d283
Freddie Rosario <rosario@ecohealthalliance.org>
2016-07-08 12:57:56 -0400

    Script to create and instantiate a mongodb container

---
+++ mongodb.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir mongodb
+cd mongodb
+
+wget https://raw.githubusercontent.com/ecohealthalliance/infrastructure/master/docker/images/mongodb/Dockerfile
+wget https://raw.githubusercontent.com/ecohealthalliance/infrastructure/master/docker/images/mongodb/mongodb.conf
+wget https://raw.githubusercontent.com/ecohealthalliance/infrastructure/master/docker/images/mongodb/run.sh
+wget https://raw.githubusercontent.com/ecohealthalliance/infrastructure/master/docker/containers/mongodb.yml
+
+docker build -t mongodb .
+docker-compose -f mongodb.yml up -d
+
