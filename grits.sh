a1de79705f0703e5d560d2ecf5e11e9276db44dd
Freddie Rosario <rosario@ecohealthalliance.org>
2016-07-12 16:45:58 -0400

    Another update to grits.sh

--- grits.sh
+++ grits.sh
@@ -19,21 +19,25 @@ if [[ ! -f grits-provisioned.tar.gz && ! -f grits-provisioned.tar ]]; then
   gzip -d grits-provisioned.tar.gz
 fi
 
+#Load the image
 docker load < grits-provisioned.tar
 
 #Get and setup config files
 export LOCAL_IP=$(ifconfig eth0|grep "inet addr"|awk -F":" '{print $2}'|awk '{print $1}')
 wget https://raw.githubusercontent.com/ecohealthalliance/grits-deploy-ansible/master/compose.yml --output-document=grits.yml
-sed -i "s/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/$LOCAL_IP/" grits.yml
-sed -i "s/image: grits/image: grits-provisioned/" grits.yml
+sed -i -r "s/(\b[0-9]{1,3}\.){3}[0-9]{1,3}\b/$LOCAL_IP/" grits.yml
+sed -i -r "s/image: grits/image: grits-provisioned/" grits.yml
 
+#Instantiate a new grits container
 docker-compose -f grits.yml up -d
 
-docker exec -t grits sed -i "s/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/$LOCAL_IP/" /var/lib/mongo/grits/grits-api/config.py
-docker exec -t grits sed -i "s/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/$LOCAL_IP/" /var/lib/mongo/grits/grits_config
-docker exec -t grits sed -i "s/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/$LOCAL_IP/" /etc/supervisor/conf.d/girderd.conf
+#Change all mongo references to use new local ip address
+docker exec -t grits find /var/lib/mongo/grits/ -type f -exec sed -i -r "s/mongodb\:\/\/(\b[0-9]{1,3}\.){3}[0-9]{1,3}\b\:27017/mongodb\:\/\/$LOCAL_IP:27017/" {} \;
+docker exec -t grits find /etc/supervisor/conf.d/ -type f -exec sed -i -r "s/mongodb\:\/\/(\b[0-9]{1,3}\.){3}[0-9]{1,3}\b\:27017/mongodb\:\/\/$LOCAL_IP:27017/" {} \;
 
-docker exec -t grits service supervisor restart
+#Restart all the services
+docker kill grits
+docker start grits
 
 echo "*****************************************************************************************"
 echo "Please update with your own bsve credentials at $GRITS_HOME/grits-api/config.py"
