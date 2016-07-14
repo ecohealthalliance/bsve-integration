6f943bd0572bd60bec3738025ad6760423e4d727
Freddie Rosario <rosario@ecohealthalliance.org>
2016-07-14 10:46:46 -0400

    Another tater update

--- tater.sh
+++ tater.sh
@@ -25,7 +25,7 @@ docker-compose -f tater.yml up -d
 
 #Settings json file
 docker exec -t tater mkdir /shared
-echo "{  "public": {    "stripe": {      "publishableKey": "pk_live_CHANGEME"    },    "analyticsSettings": {      "Google Analytics" : {"trackingId": "UA-CHANGEME"}    },    "accounts": {      "tokenUser": "bsve-user@CHANGEME.org"    }  },  "private": {    "AWS_ACCESS_KEY": "CHANGEME",    "AWS_SECRET_KEY": "CHANGEME",    "stripe": {      "secretKey": "sk_live_CHANGEME"    },    "accounts": {      "loginToken": "CHANGEME"    },    "MAIL_URL":"smtp://CHANGEME",    "jenkins": {      "url": "CHANGEME",      "user": "CHANGEME",      "key": "CHANGEME",      "buildKey": "CHANGEME"    }  }}" > settings-production.json
+echo '{  "public": {    "stripe": {      "publishableKey": "pk_live_CHANGEME"    },    "analyticsSettings": {      "Google Analytics" : {"trackingId": "UA-CHANGEME"}    },    "accounts": {      "tokenUser": "bsve-user@CHANGEME.org"    }  },  "private": {    "AWS_ACCESS_KEY": "CHANGEME",    "AWS_SECRET_KEY": "CHANGEME",    "stripe": {      "secretKey": "sk_live_CHANGEME"    },    "accounts": {      "loginToken": "CHANGEME"    },    "MAIL_URL":"smtp://CHANGEME",    "jenkins": {      "url": "CHANGEME",      "user": "CHANGEME",      "key": "CHANGEME",      "buildKey": "CHANGEME"    }  }}' > settings-production.json
 docker cp settings-production.json tater:/shared/settings-production.json
 
 echo ""
