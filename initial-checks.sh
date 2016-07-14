c3508ab0289de543b8dba1b5f6b3ebcd2eae261b
Freddie Rosario <rosario@ecohealthalliance.org>
2016-07-14 17:21:59 -0400

    Changed device to interface

--- initial-checks.sh
+++ initial-checks.sh
@@ -15,7 +15,7 @@ if [[ $1 && $2 ]]; then
   fi
 fi
 
-ifconfig $ethernet || (echo "Ethernet device named $ethernet not found. Please specify correct ethernet name with --ethernet <name>" && exit 1)
+ifconfig $ethernet || (echo "Ethernet interface named $ethernet not found. Please specify correct ethernet name with --ethernet <name>" && exit 1)
 
 docker --version || (echo "Please install docker:   https://docs.docker.com/engine/installation/linux/" && exit 1)
 
