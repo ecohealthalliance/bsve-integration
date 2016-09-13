102f664c1303de37833f50054406a43484e279c1
Nathan Breit <nabreit@gmail.com>
2016-09-13 23:30:01 +0800

    Use older DB dump date for SPA dump

--- backfill-spa-niam.sh
+++ backfill-spa-niam.sh
@@ -1,7 +1,7 @@
 # This will backfill the NIAM and SPA databases by scraping and processing
 # ProMED articles published since the database dump was created.
 sudo apt-get install -y dateutils
-database_dump_date=2016-9-6
+database_dump_date=2016-7-1
 last_n_days=`dateutils.ddiff $database_dump_date now`
 extra_vars='mongo_url=$MONGO_URL SPARQLDB_URL=$SPARQLDB_URL last_n_days='$last_n_days
 docker exec -it promed-scraper bash -c "/usr/local/bin/ansible-playbook --connection=local /promed_mail_scraper/ansible/site.yml --tags=promed,download-classifier,t11 --extra-vars \"$extra_vars\""
