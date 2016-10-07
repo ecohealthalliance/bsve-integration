d8674ec13ca7b061e3d559dbd0bc4dc746151b7d
Nathan Breit <nath@nathanathan.com>
2016-10-07 15:16:13 -0400

    geonames-api bug-fixes

--- geonames-api.sh
+++ geonames-api.sh
@@ -13,13 +13,14 @@ fi
 aws s3 cp s3://bsve-integration/elasticsearch-data.tar.gz /tmp/elasticsearch-data.tar.gz
 aws s3 cp s3://bsve-integration/geonames-api.tar.gz /tmp/geonames-api.tar.gz
 mkdir -p /mnt/elasticsearch/data
-tar -xvzf /tmp/elasticsearch-data.tar.gz /mnt/elasticsearch/data
-docker load < /tmp/geonames-api.tar.gz
+tar -xvzf /tmp/elasticsearch-data.tar.gz -C /mnt/elasticsearch/data
+gzip -d /tmp/geonames-api.tar.gz
+docker load < /tmp/geonames-api.tar
 
 elasticsearch_data_path=/mnt/elasticsearch/data
 ip_address=$(ip -4 route get 8.8.8.8 | awk '{print $7}')
 
-docker-compose -f /tmp/geonames-api.yml up -d
+docker-compose -f compose/geonames-api.yml up -d
 
 echo "*****************************************************************************************"
 echo "To change the domain name in the the API documentation,"
