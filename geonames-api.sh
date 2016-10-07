a6130c790a2671d75670775c280e6b7e5b21656e
Nathan Breit <nath@nathanathan.com>
2016-10-07 15:38:24 -0400

    add geonames-api port, fix elasticsearch db path

--- geonames-api.sh
+++ geonames-api.sh
@@ -12,20 +12,20 @@ fi
 
 aws s3 cp s3://bsve-integration/elasticsearch-data.tar.gz /tmp/elasticsearch-data.tar.gz
 aws s3 cp s3://bsve-integration/geonames-api.tar.gz /tmp/geonames-api.tar.gz
-mkdir -p /mnt/elasticsearch/data
-tar -xvzf /tmp/elasticsearch-data.tar.gz -C /mnt/elasticsearch/data
+#mkdir -p /mnt/elasticsearch/data
+tar -xvzf /tmp/elasticsearch-data.tar.gz -C /
 gzip -d /tmp/geonames-api.tar.gz
 docker load < /tmp/geonames-api.tar
 
-elasticsearch_data_path=/mnt/elasticsearch/data
-ip_address=$(ip -4 route get 8.8.8.8 | awk '{print $7}')
-
-docker-compose -f compose/geonames-api.yml up -d
+(
+  elasticsearch_data_path=/mnt/elasticsearch/data \
+  ip_address=$(ip -4 route get 8.8.8.8 | awk '{print $7}') \
+  docker-compose -f compose/geonames-api.yml up -d
+)
 
 echo "*****************************************************************************************"
-echo "To change the domain name in the the API documentation,"
-echo "edit the api config file, then restart the container like so:"
+echo "To change the domain name in the the API documentation, edit the api config file, then regenerate the docs like so:"
 echo "docker exec -it geonames-api bash"
 echo "vim apidoc.json"
-echo "docker restart geonames-api"
+echo "npm run docs"
 echo "*****************************************************************************************"
