3adfdebfe23ec88e8c5250e5498541fec5627dfb
Freddie Rosario <rosario@ecohealthalliance.org>
2016-10-18 11:26:02 -0400

    Refactored grits import process to accommodate new grits docker image

--- grits.sh
+++ grits.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ethernet="eth0"
+repo_dir=$(pwd)
 
 if [[ $1 && $2 ]]; then
   if [ "$1"=="--ethernet" ]; then
@@ -10,52 +11,40 @@ fi
 
 ./initial-checks.sh --ethernet $ethernet || exit 1
 
-#Ensure data dump file is in our directory
-if [ ! -f geonames.tar ]; then
-  aws s3 cp s3://bsve-integration/geonames.tar ./geonames.tar
-fi
-
 #Build and spin up our mongodb
 ./mongodb.sh --ethernet $ethernet
 
-#Import the geonames dataset
-ln -s $(pwd)/geonames.tar /var/log/geonames.tar
-cd /var/log/ && tar -xf geonames.tar &&\ 
-docker exec -t mongodb mongorestore --db geonames /var/log/geonames
-
 #Ensure we have a copy of the grits image
-if [[ ! -f grits-provisioned.tar.gz && ! -f grits-provisioned.tar ]]; then
-  aws s3 cp s3://bsve-integration/grits-provisioned.tar.gz ./grits-provisioned.tar.gz
-  gzip -d grits-provisioned.tar.gz
+if [[ ! -f grits.tar.gz && ! -f grits.tar ]]; then
+  aws s3 cp s3://bsve-integration/grits.tar.gz ./grits.tar.gz
+  gzip -d grits.tar.gz
 fi
 
 #Load the image
-docker load < grits-provisioned.tar
-
-export LOCAL_IP=$(ifconfig $ethernet|grep "inet addr"|awk -F":" '{print $2}'|awk '{print $1}')
-
-#Get and setup config files
-wget https://raw.githubusercontent.com/ecohealthalliance/grits-deploy-ansible/master/compose.yml --output-document=grits.yml
-sed -i -r "s/(\b[0-9]{1,3}\.){3}[0-9]{1,3}\b/$LOCAL_IP/" grits.yml
-sed -i "s/image: grits$/image: grits-provisioned/" grits.yml
-sed -i "/    \- \/mnt\/grits:\/home\/grits/d" grits.yml
+docker load < grits.tar
 
 #Instantiate a new grits container
-docker-compose -f grits.yml up -d
+cd $repo_dir &&\
+docker-compose -f compose/grits.yml up -d
 
-#Change all mongo references to use new local ip address
-docker exec -t grits find /var/lib/mongo/grits/ -type f -exec sed -i -r "s/mongodb\:\/\/(\b[0-9]{1,3}\.){3}[0-9]{1,3}\b\:27017/mongodb\:\/\/$LOCAL_IP:27017/" {} \;
-docker exec -t grits find /home/grits/ -type f -exec sed -i -r "s/mongodb\:\/\/(\b[0-9]{1,3}\.){3}[0-9]{1,3}\b\:27017/mongodb\:\/\/$LOCAL_IP:27017/" {} \;
-docker exec -t grits find /etc/supervisor/conf.d/ -type f -exec sed -i -r "s/mongodb\:\/\/(\b[0-9]{1,3}\.){3}[0-9]{1,3}\b\:27017/mongodb\:\/\/$LOCAL_IP:27017/" {} \;
-docker exec -t grits sed -i -r "s/(\b[0-9]{1,3}\.){3}[0-9]{1,3}\b:27017/$LOCAL_IP:27017/" /home/grits/diagnostic-dashboard/.scripts/autocompleteCollections.js
-docker exec -t grits sed -i -r "s/ROOT_URL=\"http\:\/\/grits.eha.io\"/ROOT_URL=\"http\:\/\/$LOCAL_IP\"/" /etc/supervisor/conf.d/dashboardd.conf
-
-#Restart all the services
-docker kill grits
-docker start grits
-
+echo "Step 1:"
 echo "*****************************************************************************************"
-echo "Please update with your own bsve credentials at /home/grits/grits-api/config.py"
-echo "Afterwards please restart the container."
+echo "In the grits container, please update all of the appropriate settings in /source-vars.sh"
+echo "HING: Look for CHANGEME"
+echo ""
+echo ""
+echo "Step 2:"
+echo "*****************************************************************************************"
+echo "Update apache config to use your SSL cert, or disable"
+echo ""
+echo ""
+echo "Step 3:"
+echo "*****************************************************************************************"
+echo "In the grits container do: 'source /source-vars.sh && /scripts/first-run.sh'"
+echo ""
+echo ""
+echo "Step 4:"
+echo "*****************************************************************************************"
+echo "Afterwards please restart the entire grits container."
+echo "You may need to manually exec in and do 'supervisorctl start all'"
 echo "Grits app will be available at http://$LOCAL_IP/new?compact=true&bsveAccessKey=loremipsumhello714902&hideBackButton=true"
-echo "*****************************************************************************************"
