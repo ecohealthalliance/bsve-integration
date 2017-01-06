d0b6ff608852576ce2bad3c41e26d784453e7528
Freddie Rosario <rosario@ecohealthalliance.org>
2017-01-06 11:07:42 -0500

    Including promed-scraper

--- eidr-connect.sh
+++ eidr-connect.sh
@@ -22,6 +22,10 @@ aws s3 cp s3://bsve-integration/spa-mongodump.tar ./spa-mongodump.tar
 ln -s $(pwd)/spa-mongodump.tar /var/log/spa-mongodump.tar
 cd /var/log/ && tar -xf spa-mongodump.tar && docker exec -t mongodb mongorestore /var/log/dump
 
+#Include promed scraper
+cd $REPO_ROOT
+./promed-scraper.sh --ethernet $ethernet
+
 #Download the docker image
 aws s3 cp s3://bsve-integration/eidr-connect.tar.gz ./eidr-connect.tar.gz
 gzip -d eidr-connect.tar.gz
