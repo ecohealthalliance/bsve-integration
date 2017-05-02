7f9e4b55dd8f88d308808cfddfe65d51a737fd1e
Freddie Rosario <rosario@ecohealthalliance.org>
2017-05-02 11:11:08 -0400

    Refactored env vars to compose file

--- grits.sh
+++ grits.sh
@@ -59,15 +59,7 @@ fi
 export AWS_CRED_FILE
 
 #Configure settings
-export LOCAL_IP=$(ifconfig $ethernet|grep "inet addr"|awk -F":" '{print $2}'|awk '{print $1}')
-inside_container sed -i "s/mongodb:\/\/CHANGEME/mongodb:\/\/$LOCAL_IP/" /source-vars.sh
-inside_container sed -i "s/redis:\/\/CHANGEME/redis:\/\/$LOCAL_IP/" /source-vars.sh
-inside_container sed -i "s/http:\/\/CHANGEME/http:\/\/$LOCAL_IP/" /source-vars.sh
-
-ECHO_STR="export BROKER_URL=redis://$LOCAL_IP:6379/0"
-inside_container bash -c "echo $ECHO_STR >> /source-vars.sh"
 inside_container sed -i "/AWS/d" /source-vars.sh
-
 inside_container mkdir /root/.aws
 docker cp $AWS_CRED_FILE grits:/root/.aws/config
 
