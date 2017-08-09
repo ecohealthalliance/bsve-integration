fcbe6cae626ab42354c9126bc3d4c741797926ec
Freddie Rosario <rosario@ecohealthalliance.org>
2017-08-09 12:47:55 -0400

    Add nuke-everything.sh script

---
+++ nuke-everything.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Remove all containers, images, and mongo data
+
+docker ps -a|awk '{print $1}'|xargs docker rm -f
+docker images|awk '{print $3}'|xargs docker rmi
+rm -fr /mnt/mongo
+
+
